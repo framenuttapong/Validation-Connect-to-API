a82adfbb843e4fadc48f111edfc9302ee4f53d7a
20♥ <nuttapong.n@ku.th>
2022-09-18 00:14:48 +0700

    first commit

---
+++ src/main/java/th/ac/ku/restaurant/controller/HomeController.java
@@ -0,0 +1,15 @@
+package th.ac.ku.restaurant.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        model.addAttribute("greeting", "Sawaddee krubbb");
+        return "home";
+    }
+}
